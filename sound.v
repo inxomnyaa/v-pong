081419a634e95e08aaaccd57e60929790b148d22
thebigsmileXD <thebigsmilexd@gmail.com>
2019-12-30 23:39:14 +0100

    try mp3 format instead

--- sound.v
+++ sound.v
@@ -7,16 +7,16 @@ import miniaudio as ma
 fn main() {
 	os.clear()
 	//load sounds
-    wav_file := 'click.wav'
-    println('Loading wav')
-    mut a := ma.from(wav_file)
+    //wav_file := 'click.wav'
+    //println('Loading wav')
+    //mut a := ma.from(wav_file)
+    mp3_file := 'click.mp3'
+    println('Loading mp3')
+    mut a := ma.from(mp3_file)
     length := int(a.length())
 
-    println('Playing wav '+length.str())
-    a.play()
-    time.sleep_ms(length)
-
-    println('Playing wav '+length.str())
+    //println('Playing wav '+length.str())
+    println('Playing mp3 '+length.str())
     a.play()
     time.sleep_ms(length)
 
